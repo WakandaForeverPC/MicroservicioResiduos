5ba2269f7a288d40cfcb7f0ca87098e27ae969c7
Samuu10 <svallmun@myuax.com>
2024-12-10 18:27:53 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microservicioresiduos/MicroservicioResiduosApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioresiduos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioResiduosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioResiduosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microservicioresiduos/MicroservicioResiduosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioresiduos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioResiduosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
